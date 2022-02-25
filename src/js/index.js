57fa3a57d39b186b46e77220193a2742f725a180
EGHCh <63925946+EGHCh@users.noreply.github.com>
2022-02-25 20:14:36 -0300

    Se crea carousel para desplegar temperatura, viento y presión atmosferica del ultimo reporte

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-const averageWeather = document.querySelector('.main__weather')
+const averageWeather = document.querySelector(`.weather`)
+const averageWind = document.querySelector(`.wind`)
+const averagePressure = document.querySelector(`.pressure`)
 const lastSol = document.querySelector(`.date__sol`)
 const smplDay = document.querySelector(`.date__day`)
 const smplMonth = document.querySelector(`.date__month`)
@@ -11,20 +13,25 @@ fetch(url)
 .then(res => res.json())
 .then(data => {
   let sol = data[`${data.sol_keys[data.sol_keys.length - 1]}`] //Obtiene la información del ultimo Sol en forma de objeto
-  // console.log(sol.AT.av) //Devuelve la t° media del día
-  // console.log(data.sol_keys[data.sol_keys.length - 1])//Devuelve el el ultimo Sol disponible como String
-  let av = parseInt(sol.AT.av) //Devuelve temperatura media del día como Int.
-  // console.log(av)
+
   let pWeather = document.createElement(`p`)
-  pWeather.innerHTML = `${av}<span>c</span>`
+  pWeather.innerHTML = `${parseInt(sol.AT.av)}<span>c</span>`
   averageWeather.appendChild(pWeather)
+
+  let pWind = document.createElement(`p`)
+  pWind.innerHTML =  `${sol.HWS.av}<span>m/s</span>`
+  averageWind.appendChild(pWind)
+
+  let pPressure = document.createElement(`p`)
+  pPressure.innerHTML =  `${parseInt(sol.PRE.av)}<span>Pa</span>`
+  averagePressure.appendChild(pPressure)
 
   let pSol = document.createElement(`p`)  
   pSol.className = `date__value`
   pSol.innerHTML = data.sol_keys[data.sol_keys.length - 1]
   lastSol.appendChild(pSol)
   
-  //Last sample Earth Date
+  //Last sample - Earth Date
   let date = new Date(sol[`First_UTC`])
 
   let day = date.getDate()
@@ -38,6 +45,5 @@ fetch(url)
   pMonth.className = `date__value`
   pMonth.innerHTML = month
   smplMonth.appendChild(pMonth)
-  // console.log(date)
 })
 .catch(err => console.log(err))
